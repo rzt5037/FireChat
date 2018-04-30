0483a209dc51596694f5316dc6b0c6216d3a2d22
Richard Travis <r.z.travis@gmail.com>
2018-04-29 18:46:21 -0600

    Updating package file for deployment

--- server.js
+++ server.js
@@ -76,8 +76,3 @@ server.on('response', function (request) {
 });
 
 module.exports = server;
-
-var port = process.env.PORT || 3000;
-app.listen(port, "0.0.0.0", function() {
-console.log("Listening on Port 3000");
-});
